d2de12dadb9df7182f365b220544a1f129f16e88
Shantanu Sinha <shantanu.p.sinha@gmail.com>
2024-02-16 00:10:20 -0500

    updated setup script

--- scripts/setup_container.sh
+++ scripts/setup_container.sh
@@ -1,7 +1,7 @@
 apt update && apt install -y git default-jdk python3 python3-pip wget curl tar
-mkdir tools
+mkdir -p duality/tools
 
-pushd tools
+pushd duality/tools
 
 # Install Maven
 wget -O - https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz | tar xzf -
@@ -9,14 +9,15 @@ export PATH=$PWD/apache-maven-3.9.6/bin:$PATH
 
 # Install NVM
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-nvm install --lts
-nvm use --lts
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-popd
+nvm install --lts
+nvm use --lts
+
+cd ..
 
 git clone https://github.com/rongpan/RFixer.git
 
@@ -24,3 +25,4 @@ pushd RFixer
 
 mvn install
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+ln -s $PWD/*z3* /usr/lib/
